9724dba90b5de253c80d706ef6ff9cbabe08d6bb
con40 <andrew.conforti@gmail.com>
2021-04-09 14:09:51 -0400

    Update index.php
    center

--- web/index.php
+++ web/index.php
@@ -2,7 +2,7 @@
 <head> <script async src="https://sdk.arengu.com/forms.js"></script></head>
 
 <body>
-<div data-arengu-form-id="161798400790783775"></div>
+  <center><div data-arengu-form-id="161798400790783775"></div></center>
 </body>
 
 </html>
